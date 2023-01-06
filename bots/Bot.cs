7f0406f33177685169d2b4456b177c749ae105fc
Kacpergibas <92591713+Kacpergibas@users.noreply.github.com>
2023-01-06 14:11:30 +0100

    Add files via upload

---
+++ bots/Bot.cs
@@ -0,0 +1,29 @@
+﻿using Sandbox;
+
+public class SBot : Bot
+{
+	[ConCmd.Admin( "sbot", Help = "Spawn SBot bot." )]
+	internal static void SpawnCustomBot()
+	{
+		Game.AssertServer();
+
+		// Create an instance of your custom bot.
+		_ = new SBot();
+	}
+
+	public override void BuildInput()
+	{
+		// Here we can choose / modify the bot's input each tick.
+		// We'll make them constantly attack by holding down the PrimaryAttack button.
+		Input.SetButton( InputButton.PrimaryAttack, true );
+	}
+
+	/*
+	public override void Tick()
+	{
+		// Here we can do something with the bot each tick.
+		// Here we'll print our bot's name every tick.
+		Log.Info( Client.Name );
+	}
+	*/
+}
